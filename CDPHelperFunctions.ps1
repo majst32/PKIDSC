7ca15569f9352bb5a1e333b42176ced19504582c
Melissa Januszko <majst32@users.noreply.github.com>
2017-08-15 13:14:26 -0700

    added function code

--- CDPHelperFunctions.ps1
+++ CDPHelperFunctions.ps1
@@ -1,5 +1,22 @@
-﻿#Query
+﻿function get-CACRLCDPExistence {
+[cmdletbinding()]
+    param (
+        [parameter(Mandatory=$True)]
+        [string]$URI
+        )
+
+if ((Get-CACrlDistributionPoint | Where-Object {$_.uri -eq $URI}) -ne $Null) {
+    write-verbose "CRL Distribution Point $URI exists."
+    return $True
+    }
+else {
+    write-verbose "CRL Distribution Point $URI does not exist."
+    return $False
+    }
+}
+
 function Compare-CACRLDistributionPoint {
+[cmdletbinding()]
     param (
         [parameter(Mandatory=$True)]
         [string]$URI,
@@ -11,17 +28,81 @@ function Compare-CACRLDistributionPoint {
         [boolean]$AddToCrlIDP
         )
 
-$PSBoundParameters
-Get-CACrlDistributionPoint | Where-Object {$_.uri -eq $URI}
+$X = Get-CACrlDistributionPoint | Where-Object {$_.uri -eq $URI}
+
+if ($X -ne $Null) {
+    #URI is found, now check all of the parameters
+    write-verbose "URI exists, checking for parameter matches."
+    $PSBoundParameters.Remove('URI') | out-null
+    $PSBoundParameters.Remove('Verbose') | out-null
+
+    foreach ($Item in ($PSBoundParameters.keys)) {
+        Write-Verbose "Checking if $Item is set correctly..."
+        
+        if ($X.$Item -ne $PSBoundParameters.$Item) {
+            write-verbose "$Item does not match the input parameter."
+            return $False
+            }
+
+        else {
+            Write-verbose "$Item is set correctly."
+            }
+
+        }
+    write-verbose "All current settings match the inputs."
+    return $True
+    }
+else {
+    #URI was not found
+    write-verbose "URI was not found."
+    return $False
+    }
 }
 
 
-#Add
-Add-CACrlDistributionPoint
+function Add-CACrlCDPWrapper {
+[cmdletbinding()]
+    param (
+        [parameter(Mandatory=$True)]
+        [string]$URI,
+        [boolean]$PublishToServer,
+        [boolean]$PublishDeltaToServer,
+        [boolean]$AddToCertificateCDP,
+        [boolean]$AddToFreshestCRL,
+        [boolean]$AddtoCrlCDP,
+        [boolean]$AddToCrlIDP
+        )
 
-#Modify is a remove and add
-Remove-CACrlDistributionPoint 
-Add-CACrlDistributionPoint
+$PSBoundParameters.Remove('Verbose')
+Add-CACrlDistributionPoint @PSBoundParameters -force
+}
 
-#Remove (or Ensure = Absent)
-Remove-CACrlDistributionPoint -uri $URI
+function Remove-CACrlCdpWrapper {
+[cmdletbinding()]
+    param (
+        [parameter(Mandatory=$True)]
+        [string]$URI
+        )  
+    
+Remove-CACrlDistributionPoint -uri $URI -Force
+}
+
+function Set-CACrlCdpWrapper {
+[cmdletbinding()]
+    param (
+        [parameter(Mandatory=$True)]
+        [string]$URI,
+        [boolean]$PublishToServer,
+        [boolean]$PublishDeltaToServer,
+        [boolean]$AddToCertificateCDP,
+        [boolean]$AddToFreshestCRL,
+        [boolean]$AddtoCrlCDP,
+        [boolean]$AddToCrlIDP
+        )
+
+Remove-CACrlCdpWrapper -URI $URI
+$PSBoundParameters.Remove('Verbose') | Out-Null
+
+Add-CACrlCdpWrapper @PSBoundParameters
+}
+
