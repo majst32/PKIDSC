3cd4b2396aea47167f90b38f840df2f7d101537f
Melissa Januszko <majst32@users.noreply.github.com>
2017-08-14 19:52:19 -0700

    New - CDP Helper Functions

---
+++ CDPHelperFunctions.ps1
@@ -0,0 +1,27 @@
+﻿#Query
+function Compare-CACRLDistributionPoint {
+    param (
+        [parameter(Mandatory=$True)]
+        [string]$URI,
+        [boolean]$PublishToServer,
+        [boolean]$PublishDeltaToServer,
+        [boolean]$AddToCertificateCDP,
+        [boolean]$AddToFreshestCRL,
+        [boolean]$AddtoCrlCDP,
+        [boolean]$AddToCrlIDP
+        )
+
+$PSBoundParameters
+Get-CACrlDistributionPoint | Where-Object {$_.uri -eq $URI}
+}
+
+
+#Add
+Add-CACrlDistributionPoint
+
+#Modify is a remove and add
+Remove-CACrlDistributionPoint 
+Add-CACrlDistributionPoint
+
+#Remove (or Ensure = Absent)
+Remove-CACrlDistributionPoint -uri $URI
